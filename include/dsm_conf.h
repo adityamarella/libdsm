de82a878c5b8c5aeea1028d788c0186cb129d703
pykeman <aditya.marella@gmail.com>
2015-06-23 22:19:27 -0700

    take these changes in conf

--- include/dsm_conf.h
+++ include/dsm_conf.h
@@ -4,12 +4,13 @@
 typedef struct dsm_conf_struct {
   int num_nodes;
   int master_idx;
+  int this_node_idx;
   uint8_t **hosts;
   uint32_t *ports;
 } dsm_conf;
 
 
-int dsm_conf_init(dsm_conf *c, const char *conf_file_path);
+int dsm_conf_init(dsm_conf *c, const char *conf_file_path, const char *host, uint32_t port);
 int dsm_conf_close(dsm_conf *c);
 
 #endif
